218b45f672c73b21690e617a24772e2c1b5f6817
blive0321 <BrandonHao0321@gmail.com>
2022-05-18 15:30:18 +0800

    decorator 裝飾器

--- decorator.py
+++ decorator.py
@@ -1,11 +1,11 @@
 # decorator 裝飾器本質上是一個函式，是用來輔助其它一般的函式
-# 使用decorator會先執行裝飾器內部的程式碼，再執型callback函式的程式碼
+# 使用decorator會先執行裝飾器內部的程式碼，再執行callback(callback其實就是本來的函示)函式的程式碼
 
 # ------------ 上半部 : 定義裝飾器的結構 ------------
-# def 裝飾器名稱(callback函式)
+# def 裝飾器名稱(定義callback函式名稱)
     # def 內部函式名稱():
         # callback函式()
-        # return 內部函式名稱
+    # return 內部函式名稱
 
 # ------------ 下半部 : 使用裝飾器 ------------
 # @裝飾器名稱
@@ -20,7 +20,7 @@ import time
 def timer(func):
     def wrap(sleep_time):
         t_start = time.time()
-        func(sleep_time) #callback function，就是call dosomething(sleep_time)這個函式
+        func(sleep_time) # callback function，其實就是本來的函示dosomething(sleep_time)這個函式
         t_end = time.time()
         t_count = t_end = t_start
         print('[花費時間]', t_count)
