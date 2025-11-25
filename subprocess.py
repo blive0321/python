bd73d52a6041b53fa95d5bf50fe90239e2c5a520
blive0321 <root@ip-172-31-35-33.ap-southeast-1.compute.internal>
2025-11-25 06:23:43 +0000

    Hello,AWS

--- subprocess.py
+++ subprocess.py
@@ -1,7 +1,8 @@
 #child process
 import subprocess
 
-ret = subprocess.call('route -n',shell=True)
+# ret = subprocess.call('route -n',shell=True)
+# Hello, this is Brandon from AWS!!!!!!!!!
 print(type(ret))
 print(ret)
 
